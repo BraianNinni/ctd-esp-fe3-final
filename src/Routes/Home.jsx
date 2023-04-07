372607125b17c9822f84f3c25578f0ad6cf2b885
LorenzoGazza <lorenzogazzani00@gmail.com>
2023-04-07 15:28:07 -0300

    first commit

---
+++ src/Routes/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Card from '../Components/Card'
+import { useContext } from 'react'
+import { ContextGlobal } from '../Components/utils/global.context'
+
+const Home = () => {
+
+    const {Odontologos} = useContext(ContextGlobal)
+
+    const { Theme } = useContext(ContextGlobal)
+
+    
+
+    return (
+        <div className='home' style={{background:Theme.backgroundHome, color:Theme.color}}>
+            <h1>Home</h1>
+            <div className='card-grid'>
+                {Odontologos.map(data => {
+                    return <Card key={data.id} id={data.id} name={data.name} username={data.username} show={true} />
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Home
